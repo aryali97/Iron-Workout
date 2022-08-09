b01765a2611c945ac463e82958a93df68b59cdb8
Anirudh Ryali <aryali97@gmail.com>
2022-08-08 20:48:49 -0400

    Adding workout menu button.
    Also added padding to high level layout.

--- app/src/main/java/com/example/ironworkout/MainActivity.java
+++ app/src/main/java/com/example/ironworkout/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.ironworkout;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuInflater;
+import android.view.View;
+import android.widget.PopupMenu;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void showWorkoutMenu(View view) {
+        PopupMenu popup = new PopupMenu(this, view);
+        MenuInflater inflater = popup.getMenuInflater();
+        inflater.inflate(R.menu.workout_menu, popup.getMenu());
+        popup.show();
     }
 }
